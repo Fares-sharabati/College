0dd5f054612d4e151fbfdfa3154e07b12f22d80d
Fares-sharabati <106887699+Fares-sharabati@users.noreply.github.com>
2022-06-05 20:32:44 +0300

    Delete DOCUMENTS001/Python directory

--- DOCUMENTS001/Python/Python projects/try06.py
+++
@@ -1,7 +0,0 @@
-def main():
-    total=0
-    n=int(input("enter a nonneative integer:"))
-    for x in range(1,n,+1):
-        total*=x
-    print(total)
-main()
--- DOCUMENTS001/Python/Python projects/try07.py
+++
@@ -1,7 +0,0 @@
-eng={"hi":"merhaba"}
-word = "enter a word to be translated to turkish"
-if word in eng :
-    print(word,"in turkish is:",eng[word])
-else :
-    print(word,"was not found")
-
